d2ba4083882af74683e3102651013297181feea6
LukasLokajicek <koko.lukas@seznam.cz>
2020-11-21 16:47:00 +0100

    Enable caching

--- src/main/java/cz/lokajicek/lukas/pretariffreader/PreTariffReaderApplication.java
+++ src/main/java/cz/lokajicek/lukas/pretariffreader/PreTariffReaderApplication.java
@@ -2,8 +2,10 @@ package cz.lokajicek.lukas.pretariffreader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PreTariffReaderApplication {
 
     public static void main(String[] args) {
